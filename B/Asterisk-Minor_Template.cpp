95bfa3f608b60cb6fc0fdab388953b9e2eb51542
n29-dev <nazibtalukdar29@gmail.com>
2023-08-09 02:13:40 +0600

    Solved Asterisk-Minor_Template

---
+++ B/Asterisk-Minor_Template.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+  cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+  cerr << "[ ";
+  for (auto i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+void solve()
+{
+  string a, b;
+  cin >> a >> b;
+  int al = a.length(), bl = b.length();
+
+  if (a[0] == b[0])
+  {
+    cout << "YES" << nline;
+    cout << a[0] << "*" << nline;
+    return;
+  }
+
+  if (a[al - 1] == b[bl - 1])
+  {
+    cout << "YES" << nline;
+    cout << "*" << a[al - 1] << nline;
+    return;
+  }
+
+  char c1, c2;
+  bool find = false;
+
+  for (int i = 0; i < al - 1; i++)
+  {
+    c1 = a[i];
+    c2 = a[i + 1];
+
+    for (int j = 0; j < bl - 1; j++)
+    {
+      if (b[j] == c1 && b[j + 1] == c2)
+      {
+        find = true;
+        break;
+      }
+    }
+    
+    if(find){break;}
+  }
+
+  if (find)
+  {
+    cout << "YES" << nline;
+    cout << "*" << c1 << c2 << "*" << nline;
+  }
+  else
+  {
+    cout << "NO" << nline;
+  }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("/home/nazib/Desktop/cf/Error.txt", "w", stderr);
+#endif
+  fastio();
+  int TC;
+  cin >> TC;
+
+  while (TC)
+  {
+    solve();
+    TC--;
+  }
+}
