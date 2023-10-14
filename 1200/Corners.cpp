2483bfd25a8682e82e9c7d78fc9bf008c6b09cf4
n29-dev <nazibtalukdar29@gmail.com>
2023-10-14 17:51:41 +0600

    Solved Corners

---
+++ 1200/Corners.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> v(n);
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+
+        for (int j = 0; j < m; j++)
+        {
+            if (v[i][j] == '1')
+                cnt++;
+        }
+    }
+
+    auto isOne = [](char &c)
+    {
+        if (c == '1')
+            return 1;
+        return 0;
+    };
+
+    int least = 4, tmp;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < m - 1; j++)
+        {
+            tmp = 0;
+            tmp += isOne(v[i][j]);
+            tmp += isOne(v[i + 1][j]);
+            tmp += isOne(v[i][j + 1]);
+            tmp += isOne(v[i + 1][j + 1]);
+
+            if (tmp >= 1 && tmp < least)
+                least = tmp;
+        }
+    }
+
+    debug(cnt);
+    debug(least);
+
+    if (cnt == 0)
+        cout << 0 << nline;
+    else if (least == 4)
+        cout << cnt - 2 << nline;
+    else if (least == 3)
+        cout << cnt - 1 << nline;
+    else
+        cout << cnt << nline;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("/home/nazib/Desktop/cf/Error.txt", "w", stderr);
+#endif
+    fastio();
+    int TC;
+    cin >> TC;
+
+    while (TC)
+    {
+        solve();
+        TC--;
+    }
+}
